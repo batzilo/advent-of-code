a8d50f048c6bccaf1c68c6abb9a185b7a5f1cb6f
Vasileios Souleles <vasilis.souleles@gmail.com>
2024-07-01 21:05:21 +0300

    logging

---
+++ 2022_with_c/libs/logging/include/logging.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <stdbool.h>
+#include <stddef.h>
+
+/*
+void log_info();
+
+void log_error();
+*/
---
+++ 2022_with_c/libs/logging/src/logging.c
@@ -0,0 +1,16 @@
+#include "logging.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*
+void log(const char* fmt, ...) {
+  va_list ap;
+  fprintf(stderr, fmt,
+}
+
+void log_info();
+
+void log_error();
+*/
