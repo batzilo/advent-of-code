5741bb7fc47e634f296f510def3d27b0801bc51b
Vasileios Souleles <vasilis.souleles@gmail.com>
2025-01-18 17:22:05 +0200

    template

---
+++ 2022_with_c/aoc2022/day_template/day00.c
@@ -0,0 +1,18 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "aoc2022day00.h"
+#include "dyn_array.h"
+#include "dyn_stack.h"
+
+bool solve_part_one(char* infile, int* sol) {
+  (void)infile;
+  (void)sol;
+}
+
+bool solve_part_two(char* infile, int* sol) {
+  (void)infile;
+  (void)sol;
+}
---
+++ 2022_with_c/aoc2022/day_template/driver.c
@@ -0,0 +1,32 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "aoc2022day00.h"
+
+int main(int argc, char* argv[]) {
+  if (argc < 2) {
+    fprintf(stderr, "Usage: %s <input>\n", argv[0]);
+    return EXIT_FAILURE;
+  }
+  char* infile = argv[1];
+
+  int solution = 0;
+  bool ret;
+
+  ret = solve_part_one(infile, &solution);
+  if (!ret) {
+    fprintf(stderr, "solve part one failed!\n");
+    return EXIT_FAILURE;
+  }
+  printf("%d\n", solution);
+
+  ret = solve_part_two(infile, &solution);
+  if (!ret) {
+    fprintf(stderr, "solve part two failed!\n");
+    return EXIT_FAILURE;
+  }
+  printf("%d\n", solution);
+
+  return EXIT_SUCCESS;
+}
